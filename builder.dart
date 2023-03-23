5a23dec0100896d6c19cb40ac5583d936a0b3fa1
Sergiok12 <sergioabd460@gmail.com>
2023-03-22 21:56:03 -0600

    Construccion de una instancia

---
+++ builder.dart
@@ -0,0 +1,11 @@
+class Bebida {
+  double precio;
+  String nombre;
+
+  Bebida({required this.precio, required this.nombre});
+}
+
+void main() {
+  //Se crea una instancia de la clase bebida, en este caso una muy conocida en mi pais
+  Bebida(precio: 35, nombre: "Joyita");
+}
